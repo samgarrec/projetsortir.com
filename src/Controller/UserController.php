2589da2109652827e755ab840b09dd7ba219683f
Emeline35 <emeline.balouin2019@campus-eni.fr>
2019-05-21 12:17:00 +0200

    commit 4

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Participant;
+use App\Form\ParticipantType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserController extends Controller
 {
@@ -15,5 +20,31 @@ class UserController extends Controller
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
         ]);
+    }
+
+    /**
+     * La fonction register() permet d'enregistrer un nouveau participant dans la BDD
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     * @param UserPasswordEncoderInterface $encoder
+     * @Route("/register", name="participant-register")
+     */
+    public function register (Request $request, EntityManagerInterface $em, UserPasswordEncoderInterface $encoder)
+    {
+        $participant = new Participant();
+        $registerForm =$this->createForm(ParticipantType:: class, $participant);
+        $registerForm->handleRequest ($request);
+
+        if ($registerForm->isSubmitted() && $registerForm->isValid())
+        {
+            $hashed=$encoder->encodePassword ($participant, $participant->getPassword());
+            $participant->setPassword ($hashed);
+            $em->persist($participant);
+            $em->flush();
+            $this->addFlash("success", "Vos informations ont bien été enregistrées");
+            $this->redirectToRoute("sortireni.com");
+        }
+
+        return $this->render ("user/register.html.twig", ["registerForm"=>$registerForm->createView()]);
     }
 }
